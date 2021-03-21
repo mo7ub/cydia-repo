2fcb63cda5f0384c66ead68a11b2a4086ecfad2e
Mohamed <56566800+mo7ub@users.noreply.github.com>
2021-03-21 04:28:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const options ={
     bottom: '32px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
+    right: '32px', // default: '32px'
+    left: 'unset', // default: 'unset'
     time: '0.1s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
     backgroundColor: '#fff',  // default: '#fff'
